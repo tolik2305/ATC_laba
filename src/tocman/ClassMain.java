25be6c5ed2eacd8d6d81350a9636d4f5b5c17158
tolik2305 <anatole634@gmail.com>
2019-10-16 14:51:56 +0300

    Оптимизация меню

--- src/tocman/ClassMain.java
+++ src/tocman/ClassMain.java
@@ -11,16 +11,16 @@ public class ClassMain {
     public static void main(final String[] args) {
         phoneNumbers = new PhoneNumbers(10);
 
-        phoneNumbers.add(new PhoneNumber("+380685397284", PhoneNumber.Telephone.Неизвестно));
-        phoneNumbers.add(new PhoneNumber("+380996529404", "Петров Анатолий Васильевич", "г.Одесса, ул.Филатова, 37", PhoneNumber.Telephone.Мобильный));
-        phoneNumbers.add(new PhoneNumber("+380982292557", PhoneNumber.Telephone.Неизвестно));
-        phoneNumbers.add(new PhoneNumber("+380677767677", "Иванов Иван Иванович", "г.Одесса, Старопортофранковская, 4б", PhoneNumber.Telephone.Домашний));
-        phoneNumbers.add(new PhoneNumber("+380977789987", PhoneNumber.Telephone.Неизвестно));
-        phoneNumbers.add(new PhoneNumber("+380933393393", "Петров Анатолий Фёдорович", "с.Виноградовка, ул.Ленина, 167", PhoneNumber.Telephone.Рабочий));
-        phoneNumbers.add(new PhoneNumber("+380959555955", "Иванов Пётр Иванович", "г.Киев, ул.Мира, 73г", PhoneNumber.Telephone.Мобильный));
-        phoneNumbers.add(new PhoneNumber("+380966669686", PhoneNumber.Telephone.Неизвестно));
-        phoneNumbers.add(new PhoneNumber("+380995939333", PhoneNumber.Telephone.Неизвестно));
-        phoneNumbers.add(new PhoneNumber("+380995939339", "Волков Иван Степанович", "г.Одесса, ул.Пестеля, 2а", PhoneNumber.Telephone.Неизвестно));
+        phoneNumbers.add(new PhoneNumber("+380(68)539-72-84", PhoneNumber.Telephone.Неизвестно));
+        phoneNumbers.add(new PhoneNumber("+380(99)652-94-04", "Петров Анатолий Васильевич", "г.Одесса, ул.Филатова, 37", PhoneNumber.Telephone.Мобильный));
+        phoneNumbers.add(new PhoneNumber("+380(98)229-25-57", PhoneNumber.Telephone.Неизвестно));
+        phoneNumbers.add(new PhoneNumber("+380(67)776-76-77", "Иванов Иван Иванович", "г.Одесса, Старопортофранковская, 4б", PhoneNumber.Telephone.Домашний));
+        phoneNumbers.add(new PhoneNumber("+380(97)778-99-87", PhoneNumber.Telephone.Неизвестно));
+        phoneNumbers.add(new PhoneNumber("+380(93)339-33-93", "Петров Анатолий Фёдорович", "с.Виноградовка, ул.Ленина, 167", PhoneNumber.Telephone.Рабочий));
+        phoneNumbers.add(new PhoneNumber("+380(95)955-59-55", "Иванов Пётр Иванович", "г.Киев, ул.Мира, 73г", PhoneNumber.Telephone.Мобильный));
+        phoneNumbers.add(new PhoneNumber("+380(96)666-96-86", PhoneNumber.Telephone.Неизвестно));
+        phoneNumbers.add(new PhoneNumber("+380(99)593-93-33", PhoneNumber.Telephone.Неизвестно));
+        phoneNumbers.add(new PhoneNumber("+380(99)593-93-39", "Волков Иван Степанович", "г.Одесса, ул.Пестеля, 2а", PhoneNumber.Telephone.Неизвестно));
 
         Menu();
     }
@@ -29,39 +29,90 @@ public class ClassMain {
      * Основное меню
      */
     public static void Menu(){
-        int menuNumber = 0;
+        int menuNumber;
         Scanner scanner = new Scanner(System.in);
-        while(menuNumber==0){
+        while(true){
             System.out.println("Выберите нужный пункт: " +
                     "\n1) Сведения об абоненте по его телефонному номеру" +
                     "\n2) Поиск телефонного номера по фамилии" +
                     "\n3) Изменение владельца телефонного номера" +
                     "\n4) Список свободных номеров" +
                     "\n5) Список всех номеров" +
-                    "\n6) Выход" +
+                    "\n0) Выход" +
                     "\nВведите цифру нужного пункта и нажмите Enter: ");
             boolean isCorrectly = false;
-            String str="";
+            String tempMenuNumber="";
             while (!isCorrectly) {
-                str = scanner.next();
+                tempMenuNumber = scanner.next();
                 scanner.nextLine();
-                if(str.matches("[1-6]{1}")){
+                if(tempMenuNumber.matches("[1-5,0]{1}")){
                     isCorrectly=true;
                 }
                 else {
                     System.out.println("Введите корректное значение: ");
                 }
             }
-            menuNumber = Integer.parseInt(str);
-            switch (menuNumber){
-                case 1: phoneNumbers.getDataByNumber(); break;
-                case 2: phoneNumbers.getDataByFullName(); break;
-                case 3: phoneNumbers.reassignementOfOwnership(); break;
-                case 4: phoneNumbers.listOfAvailableNumbers(); break;
-                case 5: phoneNumbers.print(); break;
-                case 6: System.exit(0); break;
+            menuNumber = Integer.parseInt(tempMenuNumber);
+            switch (menuNumber) {
+                case 1: {
+                        isCorrectly = false;
+                        while (!isCorrectly) {
+                            String number;
+                            System.out.println("Введите нужный номер телефона в формате +Х(YY)ZZZ-ZZ-ZZ где вместо X - код страны, Y - код оператора, Z - номер телефона: ");
+                            number = scanner.next();
+                            if (number.matches("[+][0-9]{1,4}[(][0-9]{2}[)][0-9]{3}[-][0-9]{2}[-][0-9]{2}")) {
+                                System.out.println(phoneNumbers.getDataByNumber(number));
+                                isCorrectly = true;
+                            } else {
+                                System.out.println("Номер введён в неверном формате, повторите ввод: ");
+                            }
+                        }
+                    }
+                break;
+                case 2: {
+                        isCorrectly = false;
+                        while (!isCorrectly) {
+                            System.out.println("Введите фамилию абонента: ");
+                            String Surname = scanner.next();
+                            if (Surname.matches("[А-Я][а-я]{2,}")) {
+                                System.out.println(phoneNumbers.getDataByFullName(Surname));
+                                isCorrectly = true;
+                            } else {
+                                System.out.println("Фамилия введена неверном формате, повторите ввод: ");
+                            }
+                        }
+                    }
+                break;
+                case 3: {
+                        isCorrectly = false;
+                        String number;
+                        while (!isCorrectly) {
+                            System.out.println("Введите номер, которому хотите поменять владельца: ");
+                            number = scanner.next();
+                            scanner.nextLine();
+                            if (number.matches("[+][0-9]{1,4}[(][0-9]{2}[)][0-9]{3}[-][0-9]{2}[-][0-9]{2}")) {
+                                if (phoneNumbers.IsInList(number)) {
+                                    System.out.println(phoneNumbers.getDataByNumber(number));
+                                    System.out.println("Введите Ф.И.О. нового владельца: ");
+                                    String fullname = scanner.nextLine();
+                                    phoneNumbers.reassignementOfOwnership(number, fullname);
+                                    isCorrectly = true;
+                                }
+                            } else {
+                                System.out.println("Номер введён в неверном формате, повторите ввод: ");
+                            }
+                        }
+                    }
+                break;
+                case 4: {
+                        System.out.println(phoneNumbers.listOfAvailableNumbers());
+                    }
+                break;
+                case 5: {
+                    System.out.println(phoneNumbers.toString());
+                } break;
+                case 0: System.exit(0); break;
             }
-
         }
     }
 }
